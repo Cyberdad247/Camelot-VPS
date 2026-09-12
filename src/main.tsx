db9acd247f0e05d28d34fc2036712d5483ab4151
Cyberdad247 <cyberdad247@gmail.com>
2026-09-12 17:02:00 -0400

    Render scroll-driven Battle Mode command center

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import { BifrostScrollExperience } from './components/BifrostScrollExperience';
-import { BattleModeCommandCenter } from './components/BattleModeCommandCenter';
+import { BattleModeScrollCommandCenter } from './components/BattleModeScrollCommandCenter';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <BifrostScrollExperience />
-    <BattleModeCommandCenter />
+    <BattleModeScrollCommandCenter />
   </StrictMode>,
 );
