0c79d9bea47767e6cea8825b5b2591a0be2e553d
Cyberdad247 <cyberdad247@gmail.com>
2026-09-12 18:16:44 -0400

    feat(ui): mount full sovereign terminal command center

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import { BifrostScrollExperience } from './components/BifrostScrollExperience';
 import { BattleWorldAssimilation } from './components/BattleWorldAssimilation';
+import { SovereignTerminalCommandCenter } from './components/SovereignTerminalCommandCenter';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
@@ -10,5 +11,6 @@ createRoot(document.getElementById('root')!).render(
     <App />
     <BifrostScrollExperience />
     <BattleWorldAssimilation />
+    <SovereignTerminalCommandCenter />
   </StrictMode>,
 );
