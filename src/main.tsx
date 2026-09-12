545828f6430c2bef358354d78f5b695d4b7710b2
Cyberdad247 <cyberdad247@gmail.com>
2026-09-12 18:26:03 -0400

    Mount Multivoice-inspired Throne Room voice council

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from './App.tsx';
 import { BifrostScrollExperience } from './components/BifrostScrollExperience';
 import { BattleWorldAssimilation } from './components/BattleWorldAssimilation';
 import { SovereignTerminalCommandCenter } from './components/SovereignTerminalCommandCenter';
+import { ThroneRoomVoicePortal } from './components/ThroneRoomVoicePortal';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <ThroneRoomVoicePortal />
     <BifrostScrollExperience />
     <BattleWorldAssimilation />
     <SovereignTerminalCommandCenter />
