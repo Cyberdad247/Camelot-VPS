38e16e8b546303f85238aebfed61d4ad33fdde61
Cyberdad247 <cyberdad247@gmail.com>
2026-09-12 19:32:12 -0400

    Mount Shadow Subspace portal in Camelot root

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import { ThroneRoomVoicePortal } from './components/ThroneRoomVoicePortal';
+import { ShadowSubspacePortal } from './components/ShadowSubspacePortal';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <ThroneRoomVoicePortal />
+    <ShadowSubspacePortal />
   </StrictMode>,
 );
