b568f5982acab90198c9a30cb3f8b52c674e6189
Cyberdad247 <cyberdad247@gmail.com>
2026-09-12 17:11:44 -0400

    Mount assimilated Battle World director

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import { BifrostScrollExperience } from './components/BifrostScrollExperience';
-import { BattleModeScrollCommandCenter } from './components/BattleModeScrollCommandCenter';
+import { BattleWorldAssimilation } from './components/BattleWorldAssimilation';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <BifrostScrollExperience />
-    <BattleModeScrollCommandCenter />
+    <BattleWorldAssimilation />
   </StrictMode>,
 );
