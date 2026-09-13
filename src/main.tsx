af45557ad652c15a3dc92581ab8c683d006b76a8
Cyberdad247 <cyberdad247@gmail.com>
2026-09-12 23:47:51 -0400

    feat(experience): mount all Camelot surfaces on shared runtime provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import { ThroneRoomVoicePortal } from './components/ThroneRoomVoicePortal';
 import { ShadowSubspacePortal } from './components/ShadowSubspacePortal';
+import { CamelotRuntimeProvider } from './runtime/CamelotRuntimeContext';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <ThroneRoomVoicePortal />
-    <ShadowSubspacePortal />
+    <CamelotRuntimeProvider>
+      <App />
+      <ThroneRoomVoicePortal />
+      <ShadowSubspacePortal />
+    </CamelotRuntimeProvider>
   </StrictMode>,
 );
