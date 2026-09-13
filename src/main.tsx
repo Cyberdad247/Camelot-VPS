319ea31f2353747c9ead45bd250691052d0e35a9
Cyberdad247 <cyberdad247@gmail.com>
2026-09-13 00:33:58 -0400

    Mount authoritative runtime HUD with command center

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import { ThroneRoomVoicePortal } from './components/ThroneRoomVoicePortal';
 import { ShadowSubspacePortal } from './components/ShadowSubspacePortal';
+import { AuthoritativeRuntimeHud } from './components/AuthoritativeRuntimeHud';
 import { CamelotRuntimeProvider } from './runtime/CamelotRuntimeContext';
 import './index.css';
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CamelotRuntimeProvider>
       <App />
+      <AuthoritativeRuntimeHud />
       <ThroneRoomVoicePortal />
       <ShadowSubspacePortal />
     </CamelotRuntimeProvider>
